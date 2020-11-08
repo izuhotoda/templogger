c3f379ccdedf7b84aca8a031e9272ab68eeec745
arnet <izuhotoda@gmail.com>
2020-11-08 17:08:42 -0300

    chartjs one line

--- public/scripts/graph.js
+++ public/scripts/graph.js
@@ -1,103 +1,29 @@
-/*
-		let request = new XMLHttpRequest();
-		var ctx = document.getElementById('myChart').getContext('2d');
-		let something = 0
-
-		request.open('GET', 'https://julia004.herokuapp.com/api/');
-		request.responseType = 'json';
-		let data = []
+const ctx = document.getElementById('myChart')
+dt = {
+	datasets:[{
+		data:[],
+		borderColor: 'rgba(36, 47, 64, 0.5)',
+		backgroundColor: 'rgba(54, 54, 54, 0.2)'
 
 
-		let myVar = setInterval(myTimer, 10000)
+	}],
+}
 
-		function myTimer(){
-			console.log(something++);
-			data = []
-			request.open('GET', 'https://julia004.herokuapp.com/api/');
-			request.send();
+for( i = 0; i < 50; i++){
+	dt.datasets[0].data.push({x: i, y: Math.random()})
+}
+console.log(dt)
+
+const chart = new Chart(ctx,{
+	type:'line',
+	data: dt,
+	options:{
+		mantainAspectRatio: false,
+		scales: {
+			xAxes: [{
+				type:'linear',
+				position:'bottom'
+			}]
 		}
-
-		request.onload = function(){
-			for (o of request.response)
-				data.push({t: moment(o.time), y: o.temp});
-
-			var myChart = new Chart(ctx, {
-				type: 'line',
-				data: {
-					datasets: [{
-						label: 'Sempron',
-						data:data
-					}]
-				},
-				options: {
-					scales: {
-				/*	yAxes: [{
-						ticks: {
-							beginAtZero: true
-						}
-					}],
-					*/	/*xAxes: [{
-						type: 'time',
-						time: {unit:'minute'},
-					}]
-				}
-			}
-		});
-		}
-
-		request.send();
-*/
-var ctx = document.getElementById('myChart').getContext('2d');
-
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 9, 3, 5, 2, 3, 12, 9, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-//		responsive : true,
-//		maintainAspectRatio: false,
-		title: {
-			display: true,
-			text: 'Solo me falta esto'
-
-		},
-		legend: {
-			position: 'right',
-			onClick: (text)=>{
-				alert('click on legend'),
-				text
-
-			}},
-//    	aspectRatio: 5,
-	
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+	}
+})
