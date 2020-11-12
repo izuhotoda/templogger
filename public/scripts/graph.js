e66cafd5ee1b95fdcadad398f171404ef98f56fb
arnet <izuhotoda@gmail.com>
2020-11-11 23:14:51 -0300

    mongod consult + random walker

--- public/scripts/graph.js
+++ public/scripts/graph.js
@@ -7,21 +7,8 @@ dt = {
 		backgroundColor: 'rgba(54, 54, 54, 0.2)'
 	}]
 }
-//////////////////////////////////////////////////////////
-async function getTemps(){
-	const response = await fetch('http://localhost:3000/api')
-	const json = await response.json()
-	return json
-}
-//////////////////////////////////////////////////////////
-getTemps()
-.then(temps=>{
-	temps.forEach((temp, index)=>{	
-		dt.datasets[0].data.push({x: index, y: temp.value})
-	})
-})
-.then(_=>{
-	const chart = new Chart(ctx,{
+
+const chart = new Chart(ctx,{
 		type:'line',
 		data: dt,
 		options:{
@@ -34,6 +21,41 @@ getTemps()
 			}
 		}
 	})
+
+
+const btn = document.getElementById('clickMe')
+function addData(){
+	chart.data.datasets.forEach((dataset)=>{
+		dataset.label = dataset.data.length
+		dataset.data.push({
+			x: dataset.data.length, 
+			y: dataset.data[dataset.data.length-1].y + (Math.random()*2-1)*2})
+	})
+	chart.update()
+}
+
+btn.addEventListener('click',()=>{
+	addData()	
+})
+
+setInterval(addData, 500)
+
+//////////////////////////////////////////////////////////
+async function getTemps(){ 
+	const response = await fetch('http://192.168.0.26:3000/api')
+	const json = await response.json()
+	return json
+}
+//////////////////////////////////////////////////////////
+
+getTemps()
+.then(temps=>{
+	temps.forEach((temp, index)=>{	
+		dt.datasets[0].data.push({x: index, y: temp.value})
+	})
+})
+.then( _=> {
+	chart.update()
 })
 .catch(error => {
 	console.log('NOK:\n' + error)
